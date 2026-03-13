f9b5ce85eb09561499ff28b13b854aa1263a4913
Kendaka <barengkendrick4@gmail.com>
2026-03-13 22:19:12 +0400

    creatd lines

--- Algorithms/Recursion/CollatzConjecture.py
+++ Algorithms/Recursion/CollatzConjecture.py
@@ -1,13 +1,3 @@
-def col(n):
-    if n == 1:
-        return 0
-    elif n % 2 == 0:
-        return 1 + col(n//2)
-    else:
-        return 1 + col(3*n + 1)
-    
-print(col(4))
-
 # Lift your skinny fists like antennas to heaven - Godspeed You! Black Emperor
 # Atrocity Exhibition - Danny Brown
 # To Pimp a Butterfly - Kendrick Lamar
@@ -19,6 +9,7 @@ print(col(4))
 # Deathconsciousness - Have a Nice Life
 # Hounds of Love - Kate Bush
 # Bonito Generation - Kero Kero Bonito
+# In the Aeroplane Over the Sea - Neutral Milk Hotel
 # Madvillainy - Madvillain
 # OK Computer - Radiohead
 # Abbey Road - The Beatles
@@ -27,27 +18,41 @@ print(col(4))
 # House of Balloons - The Weeknd
 # The Glow Pt. 2 - The Microphones
 # The Disintegration - The Cures
+# The Queen is Dead - The Smiths
+# Wish You Were Here - Pink Floyd
+# Ants from Up There - Black Country, New Road
+# Pet Sounds - The Beach Boys
+# Bottomless Pit - Death Grips
 # LP1 - FKA Twigs
 # Helpessness Blues - Fleet Foxes
-# MAster of Puppets - Metallica
+# Elextric Ladyland - Jimi Hendrix
+# Physical Graffiti - Led Zeppelin
+# Sinners Get Ready - Lingua Ignota
+# Things fall apart - The Roots
+# Master of Puppets - Metallica
 # Paranoid - Black Sabbath
 # Reasonable Doubt - Jay-Z
 # We got it from Here... Thank You 4 Your service - A Tribe Called Quest
 # The Miseducation of Lauryn Hill - Lauryn Hill
+# The Black Saint and the Sinner Lady - Charles Mingus
 # Saturation - Brockhampton
 # LP! - JPEGMAFIA
 # Rodeo - Travis Scott
+# Blonde - Frank Ocean
 # Unknown Pleasures - Joy Division
 # Funeral - Arcade Fire
+# Illmatic - Nas
 # Voodoo - D'Angelo
 # The Rise and Fall of Ziggy Stardust and the Spiders from Mars - David Bowies
 # Emotion - Carly Rae Jepsen
 # Goo - Sonic Youth
 # Live Love A$AP - A$AP Rocky
 # The Life of Pablo - Kanye West
+# Hell Hath No Fury - Clipse
 # Carrie and Lowell - Sufjan Stevens
 # Dragon New Warm Mountain I Believe in You - Big Thief
 # Mama's Gun - Erykah Badu
 # Third - Portishead
+# Jeffrey - Young thug
 
 
