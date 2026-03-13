98550dbeb0ac60c824c3e83cbe2f3a4a3148c9ab
Kendaka <barengkendrick4@gmail.com>
2026-03-13 22:11:52 +0400

    added trac

--- Algorithms/Recursion/CollatzConjecture.py
+++ Algorithms/Recursion/CollatzConjecture.py
@@ -12,20 +12,42 @@ print(col(4))
 # Atrocity Exhibition - Danny Brown
 # To Pimp a Butterfly - Kendrick Lamar
 # Downward Spiral - Nine Inch Nails
+# Purple Rain - Prince
 # 36 Chambers - Wu-Tang Clan
 # Vespertine - Bjork
 # In the court of the crimson king - King Crimson
-# House of Balloons - The Weeknd
+# Deathconsciousness - Have a Nice Life
+# Hounds of Love - Kate Bush
 # Bonito Generation - Kero Kero Bonito
 # Madvillainy - Madvillain
 # OK Computer - Radiohead
 # Abbey Road - The Beatles
 # Teen Dream - Beach House
+# Highway 61 Revisited - Bob Dylan
+# House of Balloons - The Weeknd
 # The Glow Pt. 2 - The Microphones
 # The Disintegration - The Cures
 # LP1 - FKA Twigs
+# Helpessness Blues - Fleet Foxes
 # MAster of Puppets - Metallica
+# Paranoid - Black Sabbath
+# Reasonable Doubt - Jay-Z
+# We got it from Here... Thank You 4 Your service - A Tribe Called Quest
+# The Miseducation of Lauryn Hill - Lauryn Hill
 # Saturation - Brockhampton
+# LP! - JPEGMAFIA
+# Rodeo - Travis Scott
+# Unknown Pleasures - Joy Division
 # Funeral - Arcade Fire
+# Voodoo - D'Angelo
+# The Rise and Fall of Ziggy Stardust and the Spiders from Mars - David Bowies
+# Emotion - Carly Rae Jepsen
 # Goo - Sonic Youth
+# Live Love A$AP - A$AP Rocky
+# The Life of Pablo - Kanye West
+# Carrie and Lowell - Sufjan Stevens
+# Dragon New Warm Mountain I Believe in You - Big Thief
+# Mama's Gun - Erykah Badu
+# Third - Portishead
+
 
