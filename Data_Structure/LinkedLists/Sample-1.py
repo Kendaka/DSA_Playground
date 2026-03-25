85a47d8e2d3257e2ac11c4fcf5fbfd26d5f14996
Kendaka <barengkendrick4@gmail.com>
2026-03-25 23:19:58 +0400

    rebuilding the singly linked lists

--- Data_Structure/LinkedLists/Sample-1.py
+++ Data_Structure/LinkedLists/Sample-1.py
@@ -1,22 +1,4 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-
-# creating nodes
-node1 = Node(10)
-node2 = Node(20)
-node3 = Node(30)
-
-# linking those nodes
-node1.next = node2
-node2.next = node3
-
-# assignment of head
-head = node1
-
-# example
-current = head
-target_index = 3
-for i in range(target_index):
-    current = current.next
+class SinglyNode:
+    def __init__(self, head, next):
+        head = 0
+        self.next
