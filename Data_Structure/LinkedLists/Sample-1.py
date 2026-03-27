b03e748cd0fc005c653e3c8ca7a4de11154c28c9
Kendaka <barengkendrick4@gmail.com>
2026-03-27 21:41:59 +0400

    added another node

--- Data_Structure/LinkedLists/Sample-1.py
+++ Data_Structure/LinkedLists/Sample-1.py
@@ -1,8 +1,8 @@
-
 class SinglyNode: # creating blueprint
     def __init__(self, data, next=None): # _init_ is a constructor, self is an instance of the node
         self.data = data
         self.next = next
 
 node = SinglyNode(10)
+node = SinglyNode(20)
 print(node.data)
