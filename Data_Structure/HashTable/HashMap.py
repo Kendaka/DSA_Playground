e976c06c11a518e222f32ae88cc8b678bb39d80a
Kendaka <barengkendrick4@gmail.com>
2026-06-01 22:06:50 +0400

    added conditional statement for lookup

--- Data_Structure/HashTable/HashMap.py
+++ Data_Structure/HashTable/HashMap.py
@@ -4,3 +4,7 @@ d = {"Employee": 1, "Age": 23, "Status Point": 3}
 
 # Adding key & value pair in dictionary: O(1)
 d["arsh"] = 4
+
+# Lookup for presence key in dictionary: O(1)
+if "Employee" in d:
+    print(True)
